a217a799d5f23b9bf279fc892ea8b7bcfd52c8f4
MicroTecnologia89 <65441064+MicroTecnologia89@users.noreply.github.com>
2024-01-31 00:19:59 -0600

    Add files via upload

---
+++ SPI 1.X/build/default/production/SPImain.o.d
@@ -0,0 +1,4 @@
+build/default/production/SPImain.o:  \
+	C:/Users/flexy/Desktop/PIC\ Turotiales/PIC18F26K22/ASM/SPI\ 1.X/SPImain.asm  \
+	C:/Users/flexy/Desktop/PIC\ Turotiales/PIC18F26K22/ASM/SPI\ 1.X/RETARDOS.inc  \
+
---
+++ SPI_PD.X/build/default/production/SPI_MCP.o.d
@@ -0,0 +1,4 @@
+build/default/production/SPI_MCP.o:  \
+	C:/Users/flexy/Desktop/PIC\ Turotiales/PIC18F26K22/ASM/SPI_PD.X/SPI_MCP.asm  \
+	C:/Users/flexy/Desktop/PIC\ Turotiales/PIC18F26K22/ASM/SPI_PD.X/RETARDOS.inc  \
+
